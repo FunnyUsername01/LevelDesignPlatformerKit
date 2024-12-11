5983f485e9df0108da646e7410450b19603b78b3
FunnyUsername01 <144666876+FunnyUsername01@users.noreply.github.com>
2024-12-11 16:11:51 +0100

    New onboarding
    - created new onboarding

--- objects/coin.gd
+++ objects/coin.gd
@@ -4,7 +4,6 @@ var time := 0.0
 var grabbed := false
 var respawn_sequence_started : bool = false
 
-@export var respawn : bool = true
 @export var respawn_delay : float = 1.0
 
 # Collecting coins
@@ -13,16 +12,22 @@ func _on_body_entered(body):
 	if body.has_method("collect_coin") and !grabbed:
 		
 		body.collect_coin()
-		
 		Audio.play("res://sounds/coin.ogg") # Play sound
 		
 		$Mesh.queue_free() # Make invisible
 		$Particles.emitting = false # Stop emitting stars
 		
 		grabbed = true
+		
+		
+
+
+
+
+
+
 
 # Rotating, animating up and down
-
 func _process(delta):
 	
 	rotate_y(2 * delta) # Rotation
--- scripts/player_kill_volume.gd
+++ scripts/player_kill_volume.gd
@@ -1,6 +1,6 @@
 @tool
 extends Node3D
-@export var death_height : float = -1
+@export var death_height : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
